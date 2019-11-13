23c4a12df157510efff3626b091c2d7e720f7a36
Bartosz <bzhk911@gmail.com>
2019-11-13 02:35:27 +0100

    profile component

--- src/ProfileScreen/ProfileScreen.js
+++ src/ProfileScreen/ProfileScreen.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-import styles from '../globalStyles/globalStyles';
+import styles from '../global/styles/globalStyles';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 class ProfileScreen extends React.Component {
   static navigationOptions = {
     title: 'clipboard-list',
-    tabBarIcon: (
-      <FontAwesome5 name="clipboard-list" size={25} color="#888888" />
-    ),
+    tabBarIcon: ({focused}) => {
+      return (
+        <FontAwesome5
+          name="clipboard-list"
+          size={20}
+          color={focused ? '#fff' : '#888888'}
+        />
+      );
+    },
   };
 
   render() {
